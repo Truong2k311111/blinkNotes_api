519be56902a3b4a4f33972f007106b665c087f23
Truong2k9 <tle46495@gmail.com>
2025-05-15 11:18:32 +0700

    base64

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,6 +4,8 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import io.ktor.server.application.*
+import java.io.ByteArrayInputStream
+import java.util.Base64
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -14,10 +16,12 @@ fun Application.module() {
     configureMonitoring()
     configureRouting()
 
-    val serviceAccountStream = this::class.java.classLoader.getResourceAsStream("appmobileshop-b7ca3-firebase-adminsdk-ag2tr-15c00fcf34.json")
+    val base64Credentials = System.getenv("FIREBASE_CREDENTIALS")
+        ?: error("FIREBASE_CREDENTIALS is not set")
 
+    val credentialsStream = ByteArrayInputStream(Base64.getDecoder().decode(base64Credentials))
     val option = FirebaseOptions.builder()
-        .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
+        .setCredentials(GoogleCredentials.fromStream(credentialsStream))
         .build()
 
     FirebaseApp.initializeApp(option)
